ce424053401665cfdf8d6dc1d3c0ec1da8d73f3d
Adam Basha <110662505+Bashamega@users.noreply.github.com>
2023-11-24 08:27:24 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import { Footer } from "@/components/Footer";
 import { Nav } from "@/components/Nav";
 import { Center } from "@/components/center";
+import CreditFooter from "@/components/Credits";
 import React from "react";
+
 export default function Home(){
   return(
     <main className=" max-w-[100vw] p-10">
       <Nav></Nav>
       <Center></Center>
       <Footer></Footer>
+      <CreditFooter></CreditFooter>
     </main>
   )
 }
