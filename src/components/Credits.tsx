3e9aa35eac3bcf1ba1e09996d16872a58eb1a0d7
Adam Basha <110662505+Bashamega@users.noreply.github.com>
2023-11-24 08:29:12 +0200

    Update Credits.tsx

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const CreditFooter: React.FC = () => {
   return (
-    <footer className="bg-gray-200 p-4 text-center absolute bottom-0 w-screen">
+    <footer className="bg-gray-200 p-4 text-center fixed bottom-0 w-screen">
       <p className="text-sm text-gray-600">
         Created with{' '}
         <span role="img" aria-label="Heart">
